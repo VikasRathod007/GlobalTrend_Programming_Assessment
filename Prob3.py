ec693d0b6e55f499c47a007362d3375034c4506b
VikasRathod007 <vikeyrathod007@gmail.com>
2024-07-11 21:11:35 +0530

    changes made to Prob3

--- Prob3.py
+++ Prob3.py
@@ -4,8 +4,7 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 california = fetch_california_housing()
 X = pd.DataFrame(california.data, columns=california.feature_names)
@@ -25,20 +24,3 @@ print(f"Mean Absolute Error (MAE): {mae:.2f}")
 
 r2 = r2_score(y_test, y_pred)
 print(f"R-squared (R2 score): {r2:.2f}")
-plt.figure(figsize=(14, 6))
-
-plt.subplot(1, 2, 1)
-plt.scatter(y_test, y_pred, alpha=0.5)
-plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], "r--", lw=2)
-plt.xlabel("Actual Values")
-plt.ylabel("Predicted Values")
-plt.title("Actual vs. Predicted Values")
-
-plt.subplot(1, 2, 2)
-residuals = y_test - y_pred
-sns.histplot(residuals, kde=True, bins=30)
-plt.xlabel("Residuals")
-plt.title("Residuals Distribution")
-
-plt.tight_layout()
-plt.show()
